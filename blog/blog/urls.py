5667cfb1ed973e1f916c299a6fb8516cbd6f3cdc
Vlad <vlad.zah@ukr.net>
2025-04-12 08:58:39 +0300

    DB-1 Added static files url

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -25,5 +25,4 @@ urlpatterns = [
     path('tinymce/', include('tinymce.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
